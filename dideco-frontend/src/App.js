ea30384b33e18d1432d673e759fa7143dfe015de
RichardXD520 <richard.ortiz2101@alumnos.ubiobio.cl>
2025-10-06 23:59:17 -0300

    base dashboard

--- dideco-frontend/src/App.js
+++ dideco-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import General from './pages/General';
+import ProgramaDashboard from './pages/ProgramaDashboard';
 
 // Si no tienes el componente, crea un placeholder:
 function PanelUsuario() {
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/general" element={<General />} />
+        <Route path="/programa/:id" element={<ProgramaDashboard />} />
         <Route path="/panel-usuario" element={<PanelUsuario />} />
         {/* Redirige rutas desconocidas al login */}
         <Route path="*" element={<Navigate to="/" replace />} />
